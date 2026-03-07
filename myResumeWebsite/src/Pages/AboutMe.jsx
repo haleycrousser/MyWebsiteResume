a2c82c1ed43ada26aec0d7cf0d0f08871038315a
Haley Crousser <haleycrousser99@gmail.com>
2026-03-06 19:48:50 -0600

    Started About Me

--- myResumeWebsite/src/Pages/AboutMe.jsx
+++ myResumeWebsite/src/Pages/AboutMe.jsx
@@ -5,6 +5,14 @@ function AboutMe() {
     return(
         <div>
             <h1>Haley Crousser</h1>
+            <p1>I am a software developer with experience in React, Node.js, C based languages, Python, and SQL.
+            My goals at this point in my career include continuing my studies in C++, and creating more meaningful projects. Currently,
+            I am working on a project that uses C++ to create a simple game called Phase 10, and it's exactly how it sounds. It's just like
+            the card game, but in C++. After I get a core skeleton of the game I want to try and create visuals for it, whether that'd be through
+            a game engine for 3D models or through a 2D interface. That is currently a side project as my main project is creating a website
+            for my parents business, which is a locksmithing company. I am using React for the frontend and Node.js for the backend, and I'm
+            hoping to create a chatbot to go with it to help their customers!
+            </p1>
         </div>
     );
 }
