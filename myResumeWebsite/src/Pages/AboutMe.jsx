b73a978d614a2c855f859785652766cd042293ab
Haley Crousser <haleycrousser99@gmail.com>
2026-03-07 14:51:42 -0600

    fixed mobile

--- myResumeWebsite/src/Pages/AboutMe.jsx
+++ myResumeWebsite/src/Pages/AboutMe.jsx
@@ -76,7 +76,25 @@ function AboutMe() {
 
                  <div className="textbox">
                     <h1>Haley Crousser</h1>
-                    <p>yayyyyyy comp sci!</p>
+                    <h2>Professional Computer Science Student<br></br>at Middle Tennessee State University</h2>
+                    <p1>C++ • Python • Javascript • HTML • CSS • React<br></br></p1>
+                    <p2><br></br>I am currently in my third year of college, and I've had an interest
+                        in computers since I was a child. I started programming with HTML and 
+                        CSS in high school, and I have since expanded my skillset to include C++, 
+                        Python, and React.<br></br>
+                        My goal is to become a software engineer and create meaningful work
+                        that can cause a positive impact on peoples lives. Whether that is through
+                        game development, web development, or software development. Software created
+                        by someone who cares is important to me, and the consumers. Passion has been
+                        lost in the software industry and along the way it's become about searching
+                        "jobs that pay well" on the internet. I want to be a part of the change that
+                        inspires kids to make programs that reflect their creativity and passion, 
+                        not just programs that are "marketable". As a programmer and consumer
+                        I'd much rather support a passion project from a group of developers rather
+                        than someone just in it for a profit.<br></br>
+                        
+
+                    </p2>
                 </div>
 
 
